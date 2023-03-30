b259e4f472b0415644d98f4eb0cede03bcaac39a
Leonid <ztvlnd777@gmail.com>
2023-03-30 11:13:30 +0300

    update

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,12 +1,13 @@
 export  function fetchCountries(name) {
 
 const url = `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`;  
-    return fetch(url)
-        .then(response => {
+    
+ return fetch(url)
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error(response.status);
+            }
             return response.json();
         }).then(data => {
             return data;
-        }).catch(error => {
-            console.error(error)
-        });
-}
+        });}
